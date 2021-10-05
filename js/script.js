1f9a25d90a8df834b4a62f06160a4eb27cf8f053
V.Onipko <86495395+VladimirOnipko@users.noreply.github.com>
2021-10-05 23:15:11 +0300

    boosted

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:6,
+		speed:1000,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
+});
+
